5fc4c000fafc6e37dd7a486ac5dec927adeadc76
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2025-03-30 19:07:24 -0700

    implement parse_args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use std::env;
+
 fn main() {
-    let roll_request = rollerrs::Request {
+    let mut roll_request = rollerrs::Request {
         rolls: 1,
         sides: 20,
         with: rollerrs::With::None,
         bonus: 0,
     };
+
+    let args: Vec<String> = env::args().collect();
+
+    match rollerrs::parse_args(&mut roll_request, args[1].as_str()) {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Error: {e}");
+            return;
+        },
+    }
 
     let rolls = rollerrs::conduct_rolls(&roll_request);
 
