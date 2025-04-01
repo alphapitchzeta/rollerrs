49585632f6b29158a9ebad3d552f52e67c444e56
alphapitchzeta <112736515+alphapitchzeta@users.noreply.github.com>
2025-04-01 16:21:19 -0700

    implement bonuses feature

--- src/lib.rs
+++ src/lib.rs
@@ -20,33 +20,59 @@ pub fn parse_args(r: &mut Request, args: &str) -> Result<(), String> {
         Err(e) => return Err(e.to_string()),
     };
 
-    let caps = match main_regex.captures(args) {
+    let bonus_regex = match Regex::new(r"[d|D]\d{1,3}[a|d]?(?<sign>[+|-])(?<bonus>\d{1,3})") {
+        Ok(val) => val,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let main_caps = match main_regex.captures(args) {
         Some(values) => values,
         None => return Err("could not parse args".to_string()),
     };
 
-    let rolls: i16 = caps["rolls"].parse().unwrap_or(1);
+    let rolls: i16 = main_caps["rolls"].parse().unwrap_or(1);
 
     match rolls {
         1..=100 => (),
         _ => return Err("invalid roll value".to_string()),
     }
 
-    let sides: i16 = match &caps["sides"].parse() {
+    let sides: i16 = match &main_caps["sides"].parse() {
         Ok(val) => match *val {
-            1..=100 => *val,
+            2..=100 => *val,
             _ => return Err("invalid side value".to_string()),
         },
         Err(e) => return Err(e.to_string()),
     };
 
-    let with: With = match &caps["with"] {
+    let with: With = match &main_caps["with"] {
         "d" | "D" => With::Disadvantage,
         "a" | "A" => With::Advantage,
         _ => With::None,
     };
 
-    let bonus = 0;
+    let mut bonus = 0;
+
+    if bonus_regex.is_match(args) {
+        let bonus_caps = match bonus_regex.captures(args) {
+            Some(values) => values,
+            None => return Err("could not parse args".to_string()),
+        };
+
+        let sign = &bonus_caps["sign"];
+
+        bonus = bonus_caps["bonus"].parse().unwrap_or_default();
+
+        match bonus {
+            1..=100 => (),
+            _ => return Err("invalid bonus value".to_string()),
+        }
+
+        if sign == "-" {
+            bonus *= -1;
+        }
+    }
+
 
     r.rolls = rolls;
     r.sides = sides;
